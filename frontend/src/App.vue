9bfb2fd1daa95f3ceaacb95211e68f0a4678cb81
Sharif <ah.sharif.biz@gmail.com>
2025-07-17 12:31:30 +0330

    adding frontend and its not complete

---
+++ frontend/src/App.vue
@@ -0,0 +1,62 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue';
+import TheWelcome from './components/TheWelcome.vue';
+</script>
+
+<template>
+  <div>
+    <h1>Show data from backend</h1>
+    <p>{{ message }}</p>
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+  data() {
+    return {
+      message: '',
+    };
+  },
+  mounted() {
+    axios
+      .get('http://api.traefik.com/api/test')
+      .then((response) => {
+        this.message = response.data.message;
+      })
+      .catch((error) => {
+        console.error('Error fetching data:', error);
+      });
+  },
+};
+</script>
+
+<style scoped>
+header {
+  line-height: 1.5;
+}
+
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+
+  .logo {
+    margin: 0 2rem 0 0;
+  }
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+}
+</style>
--- readme.md
+++ readme.md
@@ -1,9 +1,11 @@
 ## Generate SSL
 
-for generating ssl code run this comand
+for generating ssl code run this comands
 
 ```
 openssl req -x509 -nodes -days 365 -newkey rsa:2048 \
     -keyout .docker/traefik/certs/local.key -out .docker/traefik/certs/local.crt \
     -config .docker/traefik/openssl.cnf
 ```
+
+is_limited_estate
